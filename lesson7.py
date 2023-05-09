4081fa72c25f149bbfefc37d913141aa6b44bc8d
HasanbasriUysal <48217@edu.raseko.fi>
2023-05-09 13:10:50 +0300

    Lesson7 HasanUysal

--- lesson7.py
+++ lesson7.py
@@ -18,4 +18,117 @@ for i in range(12):
     agent.move(FORWARD, 1)
     agent.place(LEFT)
 
+#*************************************
+agent.place(LEFT)
+for i in range(2):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(UP, 1)
+agent.place(LEFT)
+for i in range(3):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(UP, 1)
+agent.place(LEFT)
+for i in range(3):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
 
+#*************************************
+
+agent.place(LEFT) 
+for i in range(3):   
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)  
+for i in range(8):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT) 
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(8):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(8):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(3):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+
+
+agent.place(LEFT)
+for i in range(4):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(4):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(3):
+     agent.move(FORWARD, 1)
+     agent.place(LEFT)
+agent.move(BACK, 1)
+agent.turn(RIGHT)
+agent.place(LEFT)
+for i in range(3):
+     agent.move(FORWARD, 1)
+     agent.place(LEFT)
+agent.move(BACK, 1)
+agent.turn(RIGHT)
+agent.place(LEFT)
+for i in range(2):
+     agent.move(FORWARD, 1)
+     agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(5):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(7):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(5):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(BACK, 1)
+agent.turn(RIGHT)
+agent.place(LEFT)
+for i in range(3):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(8):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)
+agent.move(FORWARD, 1)    
+agent.turn(LEFT)
+agent.move(FORWARD, 1)
+for i in range(5):
+    agent.move(FORWARD, 1)
+    agent.place(LEFT)    
+agent.move(FORWARD, 1)
