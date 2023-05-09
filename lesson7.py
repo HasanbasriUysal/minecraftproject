ac4321a36c35c5bf62d2786503938ff96488ab92
HasanbasriUysal <48217@edu.raseko.fi>
2023-05-09 10:26:37 +0300

    Lesson7_HBU

---
+++ lesson7.py
@@ -0,0 +1,10 @@
+#Laita koodin alkuun aina allaoleva Header 
+#Päivitä päivämäärä sekä omat nimikirjaimet ja koodin toimintakuvaus (lesson) 
+#*****************************************************  
+# Lesson7.py 
+# @author tekija: (ensimmaisen version tekija, HBU)  
+# @since pvm: (versio pvm 9.5.2023)  
+# @version versio: (versionumero 1.0)  
+# muutos: (HBU)  
+#*****************************************************/
+
